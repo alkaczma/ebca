036538b3ab09ddd755bbf5cfac311cd0ca10a623
alkaczma <45961564+alkaczma@users.noreply.github.com>
2021-06-25 20:11:56 +0200

    Update runtestbed.sh

--- runtestbed.sh
+++ runtestbed.sh
@@ -20,8 +20,7 @@
 
 # Author: Adam L. Kaczmarek
 #If you are using this work for research purposes please cite:
-#1. Adam L. Kaczmarek, "Testbed for 3D imaging algorithms using camera arrays", Submitted, 2020.
-#2. Adam L. Kaczmarek, "3D Vision System for a Robotic Arm Based on Equal Baseline Camera Array", Journal of Intelligent & Robotic Systems, 2019.
+#1. Adam L. Kaczmarek, "3D Vision System for a Robotic Arm Based on Equal Baseline Camera Array", Journal of Intelligent & Robotic Systems, 2019.
 
 FOLDER_DATA_IN=/media/alkaczma/USBETI/data/in/
 FOLDER_DATA_OUT=/media/alkaczma/USBETI/data/out/
